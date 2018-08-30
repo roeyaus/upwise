b936e51ff996ed98dfea9ffd9bea61d428d89a87
kizzlota <kizzlota@gmail.com>
2018-08-30 14:57:32 +0300

    add class filerider and config app

---
+++ config.py
@@ -0,0 +1,9 @@
+FILE_NAME_GITHUB_USERS = 'users.txt'
+ENCODING = 'utf-8'
+HOST_DB = '127.0.0.1'
+DATABASE_NAME = 'github'
+USER = 'test_user'
+PASSWORD = 'test_user'
+TABLE_NAME = "github_repo"
+CONNECT_TIMEOUT = 60
+PORT = 5432
---
+++ filereader.py
@@ -0,0 +1,25 @@
+import os
+from os.path import join
+from config import ENCODING
+
+
+class ReadFile:
+    def __init__(self, filename):
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        self.full_path = join(file_path, filename)
+
+    @property
+    def user_list(self):
+        try:
+            users_lists = []
+            if os.path.exists(self.full_path):
+                with open(self.full_path, "r", encoding=ENCODING) as file:
+                    for line in file:
+                        user_name = line.strip('\t\n\r\s')
+                        if len(user_name) > 1:
+                            users_lists.append(user_name)
+                return users_lists
+            else:
+                raise Exception(f'Warning file named ({filename}) is missing')
+        except Exception as e:
+            print(e)
